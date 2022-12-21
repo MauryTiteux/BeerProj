d3f908a029249904f31dce2721f7c00b6720f17a
romaintrnbx <romain.troonbeeckx@gmail.com>
2022-12-21 19:40:26 +0100

    Data base

--- Php/insertbrewery.php
+++ Php/insertbrewery.php
@@ -14,10 +14,15 @@
     $pass = "";
     
     $NomBra = $_POST["NomBra"];
+    $TypBraID = $_POST["TypBraID"];
     $Adresse = $_POST["Adresse"];
     $CPBraID = $_POST["CPBraID"];
+    $CP = $_POST["CPBraID"];
+    $Villes = $_POST["ville"];
+    $Pays = $_POST["Pays"];
     $SiteWeb = $_POST["SiteWeb"];
     $Facebook = $_POST["Facebook"];
+    $Instagram = $_POST["Instagram"];
     
     try{
         //On se connecte à la BDD
@@ -25,15 +30,31 @@
         $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
         //On insère les données reçues
+                $sth = $dbco->prepare("
+            INSERT IGNORE INTO codepostal(CP, Villes)
+            VALUES(:CP, :Villes)");
+        $sth->bindParam(':CP',$CP);
+        $sth->bindParam(':Villes',$Villes);
+        $sth->execute();
+
+                $sth = $dbco->prepare("
+            INSERT IGNORE INTO pays (Pays)
+            VALUES(:Pays)");
+        $sth->bindParam(':Pays',$Pays);
+        $sth->execute();
+
         $sth = $dbco->prepare("
-            INSERT INTO Brasseries(NomBra, Adresse, CPBraID, SiteWeb, Facebook)
-            VALUES(:NomBra, :Adresse, :CPBraID, :SiteWeb, :Facebook)");
+            INSERT INTO Brasseries(NomBra, TypBraID, Adresse, CPBraID, SiteWeb, Facebook, Instagram)
+            VALUES(:NomBra, :TypBraID, :Adresse, :CPBraID, :SiteWeb, :Facebook, :Instagram)");
         $sth->bindParam(':NomBra',$NomBra);
         $sth->bindParam(':Adresse',$Adresse);
         $sth->bindParam(':CPBraID',$CPBraID);
         $sth->bindParam(':SiteWeb',$SiteWeb);
         $sth->bindParam(':Facebook',$Facebook);
+        $sth->bindParam(':Instagram',$Instagram);
+        $sth->bindParam(':TypBraID',$TypBraID);
         $sth->execute();
+
         
         //On renvoie l'utilisateur vers la page de remerciement
         header("Location:../index.html");
@@ -43,4 +64,24 @@
     }
 ?>
 		
-		</body>
+<?php
+
+    //PAGE DE CONNECTION A LA DB
+
+    $username = "root";
+    $password = "";
+    $host = "localhost";
+    $dbname = "database_bieres";
+
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+    try {
+        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
+    }
+    catch(PDOException $ex){
+        die("Failed to connect to the database: " . $ex->getMessage());
+    }
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
